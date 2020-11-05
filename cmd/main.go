6eafa853f96f1b3bad59782c529183acc02a06e7
drkchiloll <sam.womack@entropicos.com>
2020-11-05 08:32:48 -0600

    update scratchpad

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/ApogeeNetworking/gonetssh"
-	"github.com/pkg/sftp"
 	"github.com/subosito/gotenv"
 )
 
@@ -21,13 +20,18 @@ func init() {
 	enablePass = os.Getenv("ENABLE_PW")
 }
 
+func trimWS(text string) string {
+	tsRe := regexp.MustCompile(`\s+`)
+	return tsRe.ReplaceAllString(text, " ")
+}
+
 func main() {
 	dev, err := gonetssh.NewDevice(
 		sshHost,
 		sshUser,
 		sshPass,
 		enablePass,
-		gonetssh.DType.Cisco9800,
+		gonetssh.DType.Aruba8,
 	)
 	if err != nil {
 		// Device Type Not Supported
@@ -38,85 +42,7 @@ func main() {
 		log.Fatalf("%v", err)
 	}
 	defer dev.Disconnect()
-	out, _ := dev.SendCmd("show run")
+
+	out, _ := dev.SendCmd("show configuration effective | include essid")
 	fmt.Println(out)
-	fmt.Println("running it again")
-	out, _ = dev.SendCmd("show run")
-	fmt.Println(out)
-}
-
-func aireOS() {
-	dev, _ := gonetssh.NewDevice(
-		sshHost,
-		sshUser,
-		sshPass,
-		enablePass,
-		gonetssh.DType.CiscoAireos,
-	)
-	err := dev.Connect(10)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	defer dev.Disconnect()
-	cmds := []string{
-		"save config",
-	}
-	dev.SendConfig(cmds)
-}
-
-func sftpUploadFileExample() {
-	dev, _ := gonetssh.NewDevice(
-		sshHost,
-		sshUser,
-		sshPass,
-		enablePass,
-		gonetssh.DType.X86,
-	)
-	sshCfg := dev.NewClientConfig()
-	sshClient, err := dev.NewClient(sshCfg)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	scp, err := sftp.NewClient(sshClient)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	defer func() {
-		scp.Close()
-		dev.Disconnect()
-	}()
-	dstFile, err := scp.Create("gonetssh.go")
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	defer dstFile.Close()
-	srcFile, err := os.Open("gonetssh.go")
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	bytes, err := io.Copy(dstFile, srcFile)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	fmt.Printf("%d bytes copied\n", bytes)
-}
-
-func ios() {
-	dev, _ := gonetssh.NewDevice(
-		sshHost,
-		sshUser,
-		sshPass,
-		enablePass,
-		gonetssh.DType.CiscoIOS,
-	)
-	err := dev.Connect(10)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	defer dev.Disconnect()
-	res, err := dev.SendCmd("show cdp neighbor")
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	fmt.Println(res)
 }
